e9fe6a5069cddc9f4d6585067148f5bdbb66545a
SoMNuS65 <121434983+SoMNuS65@users.noreply.github.com>
2023-11-12 16:02:02 +0500

    synonims

--- Tasks/Synonyms/cin_template.cpp
+++ Tasks/Synonyms/cin_template.cpp
@@ -1,33 +1,40 @@
 #include <iostream>
+#include <map>
+#include <set>
+#include <string>
 
-int main()
-{
-    // some container
-    int N = 0;
-    std::cin >> N;
-    for (int i = 0; i < N; ++i)
-    {
-        std::string command;
-        std::cin >> command;
-        if (command == "ADD")
-        {
-            std::string word1, word2;
-            std::cin >> word1 >> word2;
-            // logic
+int main() {
+		std::map<std::string, std::set<std::string>> synonims_Dict;
+        int n {0};
+        std::cin >> n;
+
+        for(size_t i {0}; i < n; i++) {
+        	std::string command;
+        	std::cin >> command;
+        	
+        	if(command == "ADD") 
+            {
+        		std::string word1, word2;
+            	std::cin >> word1 >> word2;
+            	synonims_Dict[word1].insert(word2);
+            	synonims_Dict[word2].insert(word1);
+            }
+        
+        	else if(command == "COUNT") 
+        	{
+        		std::string word;
+            	std::cin >> word;
+            	std::cout << synonims_Dict[word].size() << std::endl;
+        	}
+        
+        	else if(command == "CHECK") 
+        	{
+        		std::string word1, word2;
+            	std::cin >> word1 >> word2;
+            	if(synonims_Dict[word1].find(word2) != synonims_Dict[word1].end())
+                	std::cout << "YES" << std::endl;
+            	else
+                	std::cout << "NO" << std::endl;
+        	}
         }
-        else if (command == "COUNT")
-        {
-            std::string word;
-            int num;
-            std::cin >> word >> num;
-            // logic
-        }
-        else if (command == "CHECK")
-        {
-            std::string word;
-            int num;
-            std::cin >> word >> num;
-            // logic
-        }
-    }
 }
